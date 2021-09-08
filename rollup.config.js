07b401c617399848f0f2f8bb7323d2f762c1caa0
Ernesto Resende <ernestoresende@hotmail.com>
2021-09-08 15:35:42 -0300

    build: added bundler configuration files

---
+++ rollup.config.js
@@ -0,0 +1,32 @@
+import { babel } from "@rollup/plugin-babel";
+import resolve from "@rollup/plugin-node-resolve";
+import copy from "rollup-plugin-copy";
+import external from "rollup-plugin-peer-deps-external";
+
+export default {
+  input: "src/index.js",
+  output: {
+    file: "dist/index.js",
+    format: "umd",
+    name: "ReactWebMask",
+    exports: "named",
+    globals: {
+      react: "React",
+    },
+  },
+  plugins: [
+    external(),
+    copy({
+      targets: [
+        {
+          src: "src/masks/utils",
+          dest: "dist/",
+        },
+      ],
+    }),
+    resolve({ jsnext: true }),
+    babel({
+      babelHelpers: "bundled",
+    }),
+  ],
+};
